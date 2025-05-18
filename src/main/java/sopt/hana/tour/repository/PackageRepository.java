9885bffcf9176d6903fe8e84acac2f1fd823d8fb
조민혁 <71816821+minhyuk2@users.noreply.github.com>
2025-05-19 03:48:54 +0900

    feat : package repository 생성 #3

---
+++ src/main/java/sopt/hana/tour/repository/PackageRepository.java
@@ -0,0 +1,8 @@
+package sopt.hana.tour.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PackageRepository extends JpaRepository<Package,Long> {
+}
