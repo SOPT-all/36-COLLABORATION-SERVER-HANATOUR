c2eec77bc5cd4289021941637f11fb5836b87689
조민혁 <71816821+minhyuk2@users.noreply.github.com>
2025-05-19 22:58:47 +0900

    fix : repository 수정 #9

--- src/main/java/sopt/hana/tour/repository/TagRepository.java
+++ src/main/java/sopt/hana/tour/repository/TagRepository.java
@@ -3,7 +3,7 @@ package sopt.hana.tour.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import sopt.hana.tour.domain.Tag;
+import sopt.hana.tour.entity.Tag;
 
 @Repository
 public interface TagRepository extends JpaRepository<Tag,Long> {
