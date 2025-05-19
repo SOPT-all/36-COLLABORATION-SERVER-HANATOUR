449d57750e3e0f3b6ed390412efce1d0573a11ce
조민혁 <71816821+minhyuk2@users.noreply.github.com>
2025-05-20 03:12:49 +0900

    feat : 컨트롤러에 filter 부분 추가  #2

--- src/main/java/sopt/hana/tour/controller/PackageController.java
+++ src/main/java/sopt/hana/tour/controller/PackageController.java
@@ -3,12 +3,17 @@ package sopt.hana.tour.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import sopt.hana.tour.common.response.ApiResponse;
 import sopt.hana.tour.dto.request.MainSearchRequest;
 import sopt.hana.tour.dto.response.MainSearchResponse;
+import sopt.hana.tour.dto.response.SearchFilterResponse;
 import sopt.hana.tour.service.DiscountRunService;
+import sopt.hana.tour.service.FilterService;
 import sopt.hana.tour.service.MainSearchService;
 import sopt.hana.tour.dto.request.PackageRequest;
 import sopt.hana.tour.dto.response.PackageResponse;
@@ -24,6 +29,7 @@ public class PackageController {
     private final MainSearchService mainSearchService;
     private final PackageService packageService;
     private final DiscountRunService discountRunService;
+    private final FilterService filterService;
 
     // 메인페이지 검색 API
     @PostMapping("/packages/search")
@@ -50,6 +56,14 @@ public class PackageController {
 	public ResponseEntity<ApiResponse<PackageResponse>> postPackage(@RequestBody PackageRequest request){
 		return  ResponseEntity.ok(ApiResponse.success(201,"패키지가 작성되었습니다.",packageService.postPackage(request)));
 	}
+
+
+    @GetMapping("/packages/filter")
+    public ResponseEntity<ApiResponse<SearchFilterResponse>> getSearch(@RequestParam Long period,
+        @PageableDefault(page = 0, size = 20)
+        Pageable pageable){
+        return ResponseEntity.ok(ApiResponse.success(200,"패키지 검색결과입니다.",filterService.getSearchFilter(period,pageable)));
+    }
 
 
 }
