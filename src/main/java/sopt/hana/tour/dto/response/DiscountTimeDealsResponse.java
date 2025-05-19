e01d267ceb6a67429b8330c39e2cd148b462aec5
조민혁 <71816821+minhyuk2@users.noreply.github.com>
2025-05-20 03:39:36 +0900

    feat : 타임특가 필터링 구현 #2

---
+++ src/main/java/sopt/hana/tour/dto/response/DiscountTimeDealsResponse.java
@@ -0,0 +1,15 @@
+package sopt.hana.tour.dto.response;
+
+import java.util.List;
+
+public record DiscountTimeDealsResponse(String title,
+												String tagName1,
+												String tagName2,
+												String tagName3,
+												Long price,
+												String imageUrl) {
+
+
+
+
+}
---
+++ src/main/java/sopt/hana/tour/service/DiscountTimeDealsService.java
@@ -0,0 +1,28 @@
+package sopt.hana.tour.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import sopt.hana.tour.dto.response.DiscountTimeDealsResponse;
+import sopt.hana.tour.entity.Package;
+import sopt.hana.tour.enums.DiscountType;
+import sopt.hana.tour.repository.PackageRepository;
+
+@RequiredArgsConstructor
+@Service
+public class DiscountTimeDealsService {
+
+	private final PackageRepository packageRepository;
+
+	public List<DiscountTimeDealsResponse> getTimeDeals(){
+
+		List<Package> packages = packageRepository.findPackagesByDiscountType(DiscountType.TIMEDEAL);
+
+		return packages.stream().map(pkg->
+			new DiscountTimeDealsResponse(pkg.getTitle(),pkg.getTags().getTagName1().toString(),pkg.getTags().getTagName2().toString(),pkg.getTags().getTagName3().toString(),
+				pkg.getPrice(),pkg.getImageUrl())).toList();
+	}
+
+}
