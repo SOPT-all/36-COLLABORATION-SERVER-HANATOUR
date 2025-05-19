bd792110679d26eebf4dfcec57ee4add8ff74784
조민혁 <71816821+minhyuk2@users.noreply.github.com>
2025-05-19 22:58:45 +0900

    fix : repository 수정 #9

--- src/main/java/sopt/hana/tour/repository/ScheduleRepository.java
+++ src/main/java/sopt/hana/tour/repository/ScheduleRepository.java
@@ -3,7 +3,7 @@ package sopt.hana.tour.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import sopt.hana.tour.domain.Schedule;
+import sopt.hana.tour.entity.Schedule;
 
 @Repository
 public interface ScheduleRepository extends JpaRepository<Schedule,Long> {
--- src/main/java/sopt/hana/tour/repository/TourConditionRepository.java
+++ src/main/java/sopt/hana/tour/repository/TourConditionRepository.java
@@ -3,7 +3,7 @@ package sopt.hana.tour.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import sopt.hana.tour.domain.TourCondition;
+import sopt.hana.tour.entity.TourCondition;
 
 @Repository
 public interface TourConditionRepository extends JpaRepository<TourCondition,Long> {
